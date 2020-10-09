8c182848f1f987ddc474760bb0bd6a73a272769e
Parth Raghav <iniento12@gmail.com>
2020-10-09 20:18:11 +0530

    Added lipsum pagelets

---
+++ website/src/pagelets/about.tsx
@@ -0,0 +1,48 @@
+import React from "react";
+
+const Pagelet = () => {
+  return (
+    <div>
+      <h1>Who we are?</h1>
+      <div>
+        Lorem Ipsum is simply dummy text of the printing and typesetting
+        industry. Lorem Ipsum has been the industry's standard dummy text ever
+        since the 1500s, when an unknown printer took NavLink galley of type and
+        scrambled it to make NavLink type specimen book. It has survived not
+        only five centuries, but also the leap into electronic typesetting,
+        remaining essentially unchanged. It was popularised in the 1960s with
+        the release of Letraset sheets containing Lorem Ipsum passages, and more
+        recently with desktop publishing software like Aldus PageMaker including
+        versions of Lorem Ipsum.
+      </div>
+      <div>
+        Contrary to popular belief, Lorem Ipsum is not simply random text. It
+        has roots in NavLink piece of classical Latin literature from 45 BC,
+        making it over 2000 years old. Richard McClintock, NavLink Latin
+        professor at Hampden-Sydney College in Virginia, looked up one of the
+        more obscure Latin words, consectetur, from NavLink Lorem Ipsum passage,
+        and going through the cites of the word in classical literature,
+        discovered the undoubtable source. Lorem Ipsum comes from sections
+        1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of
+        Good and Evil) by Cicero, written in 45 BC. This book is NavLink
+        treatise on the theory of ethics, very popular during the Renaissance.
+        The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes
+        from NavLink line in section 1.10.32.
+      </div>
+      <div>
+        It is NavLink long established fact that NavLink reader will be
+        distracted by the readable content of NavLink page when looking at its
+        layout. The point of using Lorem Ipsum is that it has NavLink
+        more-or-less normal distribution of letters, as opposed to using
+        'Content here, content here', making it look like readable English. Many
+        desktop publishing packages and web page editors now use Lorem Ipsum as
+        their default model text, and NavLink search for 'lorem ipsum' will
+        uncover many web sites still in their infancy. Various versions have
+        evolved over the years, sometimes by accident, sometimes on purpose
+        (injected humour and the like).
+      </div>
+    </div>
+  );
+};
+
+export default Pagelet;
---
+++ website/src/pagelets/index.ts
@@ -0,0 +1,6 @@
+import AboutPagelet from "./about";
+import BlockchainPagelet from "./blockchain";
+import DevelopPagelet from "./develop";
+import LegalPagelet from "./legal";
+
+export { AboutPagelet, BlockchainPagelet, DevelopPagelet, LegalPagelet };
