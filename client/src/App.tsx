849269c477204e7c4fb861136a0044681d44b103
Parth Raghav <iniento12@gmail.com>
2020-10-13 20:14:01 +0530

    Setup routes in the app

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,27 @@
 import React from "react";
+import {
+  BrowserRouter as Router,
+  Redirect,
+  Route,
+  Switch,
+} from "react-router-dom";
+import { PrivateRoute, ProxyRoute, PublicRoute } from "./routes";
+
+const WelcomeScreen = () => <h1>Welcome Screen</h1>;
+const HomeScreen = () => <h1>Home Screen</h1>;
+const SettingScreen = () => <h1>Setting Screen</h1>;
 
 function App() {
   return (
-    <div className="App">
-      <h1>Client!</h1>
+    <div className="app">
+      <Router>
+        <Switch>
+          <PublicRoute path="/welcome" component={WelcomeScreen} />
+          <PrivateRoute path="/home" component={HomeScreen} />
+          <PrivateRoute path="/settings" component={SettingScreen} />
+          <ProxyRoute path="/" to="/welcome" />
+        </Switch>
+      </Router>
     </div>
   );
 }
