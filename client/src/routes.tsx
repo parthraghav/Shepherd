4d4e5828226384265f75111a0ed06eb9d498b1ed
Parth Raghav <iniento12@gmail.com>
2020-10-13 20:12:01 +0530

    Added routes

---
+++ client/src/routes.tsx
@@ -0,0 +1,42 @@
+import React from "react";
+import { Route, Redirect } from "react-router-dom";
+
+const isLoggedIn = () => false;
+
+export const PublicRoute = ({ component: Component, ...rest }: any) => {
+  return <Route {...rest} render={(props) => <Component {...props} />} />;
+};
+
+export const PrivateRoute = ({ component: Component, ...rest }: any) => {
+  return (
+    <Route
+      {...rest}
+      render={(props) =>
+        isLoggedIn() ? <Component {...props} /> : <Redirect to="/" />
+      }
+    />
+  );
+};
+
+export const ProtectedRoute = ({
+  component: Component,
+  restricted,
+  ...rest
+}: any) => {
+  return (
+    <Route
+      {...rest}
+      render={(props) =>
+        isLoggedIn() ? (
+          !restricted ? (
+            <Component {...props} />
+          ) : (
+            <Redirect to="Home" />
+          )
+        ) : (
+          <Redirect to="/" />
+        )
+      }
+    />
+  );
+};
