9b462b48fa74a5908ee3b2e89f624a2da5632447
Parth Raghav <iniento12@gmail.com>
2020-10-13 23:55:35 +0530

    Added Welcome Screen

--- client/src/screens/welcome_screen.tsx
+++ client/src/screens/welcome_screen.tsx
@@ -1,5 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
+import { Logo } from "@components/logo";
+import Screen from "./screen";
+import "./welcome_screen.css";
+import { Button } from "@components/button";
 
-const WelcomeScreen = () => <h1>Welcome Screen</h1>;
+enum WelcomeState {
+  Uninitialized,
+  Progress,
+  Finished,
+}
+
+const WelcomeScreen = () => {
+  const [welcomeState, setWelcomeState] = useState(WelcomeState.Uninitialized);
+  return (
+    <Screen className="welcome-screen">
+      <div className="welcome-logo-container">
+        <Logo />
+      </div>
+      <div className="welcome-box">
+        <div className="welcome-desc-container">
+          <span>Share your extra dollars weekly</span>
+        </div>
+        <div>
+          <Button label="Login to continue" />
+        </div>
+      </div>
+    </Screen>
+  );
+};
 
 export default WelcomeScreen;
