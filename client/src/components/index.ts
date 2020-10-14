5154e2c6dd6dc6cf7fffbd044a4c467e6103c29a
Parth Raghav <iniento12@gmail.com>
2020-10-14 15:47:41 +0530

    Integrated components

---
+++ client/src/components/index.ts
@@ -0,0 +1,6 @@
+import { Logo } from "./logo";
+import { Button } from "./button";
+import { Loader } from "./loader";
+import { DelayUnmounting } from './delayed';
+
+export { Logo, Button, Loader, DelayUnmounting };
