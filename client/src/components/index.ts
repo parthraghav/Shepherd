3e082a0a07e1f53e4d9dabdb7b78e60c3704e85b
Parth Raghav <iniento12@gmail.com>
2020-10-14 23:15:09 +0530

    Added TransactionBox component

--- client/src/components/index.ts
+++ client/src/components/index.ts
@@ -3,5 +3,6 @@ import { Button } from "./button";
 import { Loader } from "./loader";
 import { DelayUnmounting } from './delayed';
 import { VivianBox } from './vivian_box';
+import { TransactionBox } from './transaction_box';
 
-export { Logo, Button, Loader, DelayUnmounting, VivianBox };
+export { Logo, Button, Loader, DelayUnmounting, VivianBox, TransactionBox };
---
+++ client/src/components/transaction_box.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { getCurrencySymbol, getDisplayableTime } from "@core/utils";
+
+export const TransactionBox = ({ data: transaction, ...rest }: any) => {
+  const displayableTime = getDisplayableTime(transaction.timestamp);
+  const currencySymbol = getCurrencySymbol(transaction.currency);
+  const transactionDescriptor =
+    transaction.type == "Donated" ? "You → Public" : "Public → You";
+  return (
+    <div
+      className="transaction-box"
+      style={{
+        backgroundColor:
+          transaction.type == "Donated"
+            ? "var(--primary-1)"
+            : "var(--primary-2)",
+      }}
+    >
+      <div className="transaction-info">
+        <div>
+          <span>{displayableTime}</span>
+        </div>
+        <div>
+          <span>{transactionDescriptor}</span>
+        </div>
+      </div>
+      <div className="transaction-amount">
+        <span>{currencySymbol}</span>
+        <span>{transaction.amount}</span>
+      </div>
+    </div>
+  );
+};
