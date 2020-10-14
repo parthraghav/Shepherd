0d24c74ebf6eec8ad2308df640fa019431ecb522
Parth Raghav <iniento12@gmail.com>
2020-10-14 15:47:06 +0530

    Added loader component

---
+++ client/src/components/loader.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { faCheck } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import "./styles.css";
+
+enum LoaderState {
+  Uninitialized,
+  Progress,
+  Finished,
+}
+
+interface LoaderProps {
+  state: LoaderState;
+}
+
+export const Loader = ({ state }: LoaderProps) => {
+  return (
+    <div
+      className={
+        "loader " +
+        (state == LoaderState.Progress ? "animated-progress" : "finished")
+      }
+    >
+      <FontAwesomeIcon icon={faCheck} />
+    </div>
+  );
+};
