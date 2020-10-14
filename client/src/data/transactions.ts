1c5224f577dc84cdd00ea25099fc7e7eb2715716
Parth Raghav <iniento12@gmail.com>
2020-10-14 23:15:47 +0530

    Added mock transaction data

---
+++ client/src/data/transactions.ts
@@ -0,0 +1,85 @@
+import { Transaction } from "@models";
+
+
+const Transactions : Array<Transaction> = [
+    {
+        type: "Donated",
+        timestamp: 1602685259558,
+        uuid: "30f813f65eb443464c6c193fac21a004",
+        amount: 55,
+        currency: "USD",
+        summary: {
+            type: "Donated",
+            isFirstOfKind: true,
+            redistributionCount: 918235,
+            donationMatchCount: 5283
+        }
+    },
+    {
+        type: "Received",
+        timestamp: 1601685243558,
+        uuid: "b291f55031c9b6e91473ef611b0c5456",
+        amount: 28.55,
+        currency: "USD",
+        summary: {
+            type: "Received",
+            isFirstOfKind: true,
+            donorCount: 918235,
+            didMeetDemonstratedNeed: true
+        }
+    },
+    {
+        type: "Donated",
+        timestamp: 1571685239558,
+        uuid: "43464c6c193fa30f813f65eb4c21a004",
+        amount: 68,
+        currency: "USD",
+        summary: {
+            type: "Donated",
+            isFirstOfKind: true,
+            redistributionCount: 918235,
+            donationMatchCount: 5283
+        }
+    },
+    {
+        type: "Received",
+        timestamp: 1502685234558,
+        uuid: "91f55031c9b6eb291473ef611b0c5456",
+        amount: 33.6,
+        currency: "USD",
+        summary: {
+            type: "Received",
+            isFirstOfKind: true,
+            donorCount: 918235,
+            didMeetDemonstratedNeed: true
+        }
+    },
+    {
+        type: "Donated",
+        timestamp: 1002685230558,
+        uuid: "c193fac21a00430f813f65eb443464c6",
+        amount: 100,
+        currency: "USD",
+        summary: {
+            type: "Donated",
+            isFirstOfKind: true,
+            redistributionCount: 918235,
+            donationMatchCount: 5283
+        }
+    },
+    {
+        type: "Received",
+        timestamp: 802685225558,
+        uuid: "e91473ef611b0c545b291f55031c9b66",
+        amount: 45,
+        currency: "USD",
+        summary: {
+            type: "Received",
+            isFirstOfKind: true,
+            donorCount: 918235,
+            didMeetDemonstratedNeed: true
+        }
+    },
+];
+
+export default Transactions;
