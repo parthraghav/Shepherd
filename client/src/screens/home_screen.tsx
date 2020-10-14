9ec340d26c7e9da195f7324b3fdad1e855c57fd3
Parth Raghav <iniento12@gmail.com>
2020-10-14 18:26:48 +0530

    Integrated VivianBox component

--- client/src/screens/home_screen.tsx
+++ client/src/screens/home_screen.tsx
@@ -1,5 +1,30 @@
 import React from "react";
+import Screen from "./screen";
+import "./home_screen.css";
+import { Logo, VivianBox } from "@components";
+import { faSmile } from "@fortawesome/free-solid-svg-icons";
+const SummaryVivianBox = (props: any) => (
+  <VivianBox {...props} icon={faSmile} primaryColor="1">
+    <span className="text">We have redistributed</span>
+    <div className="heading summary-heading">
+      <span className="currency">USD</span>
+      <span className="amount">3,490.38</span>
+    </div>
+  </VivianBox>
+);
 
-const HomeScreen = () => <h1>Home Screen</h1>;
+const HomeScreen = () => {
+  return (
+    <Screen>
+      <div className="home-screen">
+        <div className="app-header">
+          <Logo />
+        </div>
+        <SummaryVivianBox />
+        <div className=""></div>
+      </div>
+    </Screen>
+  );
+};
 
 export default HomeScreen;
