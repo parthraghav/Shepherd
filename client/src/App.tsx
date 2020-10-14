eb70d1105139db43a28298787fe2a71e4d19c5a0
Parth Raghav <iniento12@gmail.com>
2020-10-14 15:48:26 +0530

    Added transition to router

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,21 +1,30 @@
-import React from "react";
-import {
-  BrowserRouter as Router,
-  Redirect,
-  Route,
-  Switch,
-} from "react-router-dom";
+import React, { Component } from "react";
+import { BrowserRouter as Router, Switch } from "react-router-dom";
 import { PrivateRoute, ProxyRoute, PublicRoute } from "./routes";
 import { HomeScreen, SettingScreen, WelcomeScreen } from "./screens";
+import { Transition } from "react-transition-group";
+import "./theme/styles.css";
+
+const routes = [
+  { path: "/welcome", component: WelcomeScreen, routeType: PublicRoute },
+  { path: "/home", component: HomeScreen, routeType: PrivateRoute },
+  { path: "/settings", component: SettingScreen, routeType: PrivateRoute },
+];
 
 function App() {
   return (
     <div className="app">
       <Router>
         <Switch>
-          <PublicRoute path="/welcome" component={WelcomeScreen} />
-          <PrivateRoute path="/home" component={HomeScreen} />
-          <PrivateRoute path="/settings" component={SettingScreen} />
+          {routes.map(({ path, component: Component, routeType: Route }) => (
+            <Route key={path} exact path={path}>
+              {({ match }: any) => (
+                <Transition in={match !== null} timeout={300} unmountOnExit>
+                  <Component shouldRender={match !== null} />
+                </Transition>
+              )}
+            </Route>
+          ))}
           <ProxyRoute path="/" to="/welcome" />
         </Switch>
       </Router>
