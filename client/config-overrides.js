2ea9cc80fd2b5650ea1a9e5f37d71d3521fff902
Parth Raghav <iniento12@gmail.com>
2020-10-14 23:16:42 +0530

    Fixed import alias errors

--- client/config-overrides.js
+++ client/config-overrides.js
@@ -4,6 +4,9 @@ module.exports = function override(config) {
   alias({
     "@components": "src/components",
     "@screens": "src/screens",
+    "@models": "src/models",
+    "@data": "src/data",
+    "@core": "src/core"
   })(config);
 
   return config;
