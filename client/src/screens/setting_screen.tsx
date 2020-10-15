30a03ff724bae77c9055dc6de86463665e89098d
Parth Raghav <iniento12@gmail.com>
2020-10-15 17:09:18 +0530

    Added menu UI

--- client/src/screens/setting_screen.tsx
+++ client/src/screens/setting_screen.tsx
@@ -1,5 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
+import Screen from "./screen";
+import "./setting_screen.css";
+import { Button, Logo, VivianBox } from "@components";
+import { faGlobeAmericas } from "@fortawesome/free-solid-svg-icons";
 
-const SettingScreen = () => <h1>Setting Screen</h1>;
+const WelcomeVivianBox = (props: any) => (
+  <VivianBox {...props} icon={faGlobeAmericas} primaryColor="2">
+    <span className="text">Welcome back!</span>
+    <div className="heading welcome-heading">
+      <span>Parth</span>
+    </div>
+  </VivianBox>
+);
+
+const SettingScreen = () => {
+  return (
+    <Screen>
+      <div className="setting-screen">
+        <div className="app-header">
+          <Logo />
+        </div>
+        <WelcomeVivianBox />
+        <div className=""></div>
+        <div className="setting-button-group">
+          <Button label="Need help?" />
+          <Button label="How does it work?" />
+          <Button label="Contribute" />
+          <Button label="Legal Information" />
+          <Button label="Logout" />
+        </div>
+      </div>
+    </Screen>
+  );
+};
 
 export default SettingScreen;
