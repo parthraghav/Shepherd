c2eb9c743e130d6bc2184327adbcb23e76983447
Parth Raghav <iniento12@gmail.com>
2020-10-16 12:28:18 +0530

    Refactored FormButton component

--- client/src/components/form_button.tsx
+++ client/src/components/form_button.tsx
@@ -1,17 +1,18 @@
 import React from "react";
+import "./styles.css";
 
-export const FormButton = ({ label, prompt, hint }: any) => {
+export const FormButton = ({ label, prompt, hint, ...rest }: any) => {
   return (
-    <div>
-      <div>
-        <div>
+    <div {...rest} className="form-container form-button">
+      <div className="left-container">
+        <div className="form-label">
           <span>{label}</span>
         </div>
-        <div>
+        <div className="form-prompt">
           <span>{prompt}</span>
         </div>
       </div>
-      <div>{hint}</div>
+      <div className="right-container">{hint}</div>
     </div>
   );
 };
