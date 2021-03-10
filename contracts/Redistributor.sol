078f053a5f34e0cd26f0f55f52696cf08fbd1750
Parth Raghav <iniento12@gmail.com>
2021-03-10 15:19:59 +0530

    added [yield] method

--- contracts/Redistributor.sol
+++ contracts/Redistributor.sol
@@ -15,12 +15,34 @@ contract Redistributor {
     mapping(address => bool) public isUserEnrolled; // checks if user is enrolled in the redistribution program
     User[] public users; // list of all participating users
 
+    modifier adminOnly {
+        require(admin.addr == address(0x0) || msg.sender == admin.addr, "Not admin: You're not authorized");
+        _;
+    }
+
     constructor(string memory _adminName) {
-        // make contract initiator the admin
-        admin = User(msg.sender, _adminName, block.timestamp);
-        // add admin to the list of participating users
-        users.push(admin);
-        // mark admin as enrolled
-        isUserEnrolled[msg.sender] = true;
+        // yield to the contract creator
+        yield(msg.sender, _adminName);
+    }
+
+    /// Give admin access to new account. This is necessary
+    // in case of transfer of power. The action is irreversible.
+    function yield(address payable _newAdminAddr, string memory _newAdminName) public adminOnly {
+        admin = User(_newAdminAddr, _newAdminName, block.timestamp);
+        enroll(_newAdminAddr, _newAdminName);
+    }
+
+    /// Enroll new user
+    function enroll(address payable _userAddr, string memory _userName) public {
+        // proceed if user is not already marked as enrolled
+        if (!isUserEnrolled[_userAddr]) {
+            User memory newUser = User(_userAddr, _userName, block.timestamp);
+            // add user to the list of participating users
+            users.push(newUser);
+            // mark user as enrolled
+            isUserEnrolled[_userAddr] = true;
+        } else {
+            console.log("The User is already enrolled");
+        }
     }
 }
