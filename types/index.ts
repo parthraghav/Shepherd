00045fcf1cc292206e0061c1350bf4b5f1af466a
Parth Raghav <iniento12@gmail.com>
2021-03-10 16:11:21 +0530

    updated interfaces

--- types/index.ts
+++ types/index.ts
@@ -2,8 +2,10 @@ import { Signer } from "@ethersproject/abstract-signer";
 
 export interface Accounts {
   admin: string;
+  user: string;
 }
 
 export interface Signers {
   admin: Signer;
+  user: Signer;
 }
