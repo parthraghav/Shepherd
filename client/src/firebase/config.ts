c53b1989465e7eaec7dd08b1b49d22aa34ff76bf
Parth Raghav <iniento12@gmail.com>
2021-03-19 21:43:14 +0530

    added firebase config

---
+++ client/src/firebase/config.ts
@@ -0,0 +1,9 @@
+export const FIREBASE_CONFIG = {
+  apiKey: "AIzaSyCidYF6FG7YmbYRxKXKLgalKipX-lA1ExE",
+  authDomain: "shepherd-base.firebaseapp.com",
+  projectId: "shepherd-base",
+  storageBucket: "shepherd-base.appspot.com",
+  messagingSenderId: "64326854255",
+  appId: "1:64326854255:web:2c8e078f9a61ee7fb10f9a",
+  measurementId: "G-FLJ6T4FZDM",
+};
