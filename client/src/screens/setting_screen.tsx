186f8dcb98ab5f59ce18e73a2e1c98ab7aac3815
Parth Raghav <iniento12@gmail.com>
2021-03-26 16:30:36 +0530

    added approve dai button

--- client/src/screens/setting_screen.tsx
+++ client/src/screens/setting_screen.tsx
@@ -204,6 +204,13 @@ const SettingScreen = () => {
               window.location.href = `mailto:support@shepherd.so?subject=Issue | ${myInfo.name}-${myInfo.uid}`;
             }}
           />
+          <Button
+            label="Approve DAI"
+            onClick={() =>
+              (window.location.href =
+                "https://kovan.etherscan.io/address/0x4F96Fe3b7A6Cf9725f59d353F723c1bDb64CA6Aa#writeContract")
+            }
+          />
           <Button label="Enroll" onClick={() => enrollMe()} />
           <Button label="Logout" onClick={() => logout()} />
         </div>
