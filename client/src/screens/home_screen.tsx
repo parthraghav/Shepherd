b13ad359717b5d136cad8188ffc2481bc429b9bc
Parth Raghav <iniento12@gmail.com>
2021-03-26 16:30:03 +0530

    added donate functionality

--- client/src/screens/home_screen.tsx
+++ client/src/screens/home_screen.tsx
@@ -1,15 +1,18 @@
 import React, { useEffect, useRef, useState } from "react";
 import Screen from "./screen";
 import "./home_screen.css";
-import { FormInput, Logo, RadioButton, TransactionBox, VivianBox } from "@components";
+import { Button, FormInput, Logo, RadioButton, TransactionBox, VivianBox } from "@components";
 import { faSmile } from "@fortawesome/free-solid-svg-icons";
 import { Transaction } from "@models";
 import { TransactionBottomSheet } from "@components/transaction_bottom_sheet";
 import { useHistory, useLocation } from "react-router-dom";
 import { TimelineMax, TweenMax } from "gsap";
 import { getBaseInfo } from "@core/base";
-import { getDisplayableNumber } from "@core/utils";
+import { getCurrencySymbol, getDisplayableNumber } from "@core/utils";
 import { getMyTransactions } from "@core/transaction";
+import { getMyInfo } from "@core/user";
+import { donate } from "@core/redistributor";
+import { web3 } from "@core/web3";
 
 const SummaryVivianBox = ({ onCTAClick, ...rest }: any) => {
   const [baseInfo, setBaseInfo] = useState<any>();
@@ -36,6 +39,7 @@ const SummaryVivianBox = ({ onCTAClick, ...rest }: any) => {
 };
 
 const HomeScreen = () => {
+  const [myInfo, setMyInfo] = useState<any>();
   const [focusedTransaction, setFocusedTransaction] = useState<Transaction | null>();
   const [myTransactions, setMyTransactions] = useState<Transaction[]>([]);
   const [isModalFocused, setModalFocus] = useState<boolean>(false);
@@ -79,6 +83,11 @@ const HomeScreen = () => {
       setMyTransactions(myTransactions);
       console.log(myTransactions);
     })();
+    (async function fetchUserInfo() {
+      const fetchedUserInfo = await getMyInfo();
+      setMyInfo(fetchedUserInfo);
+      console.log(fetchedUserInfo);
+    })();
   }, []);
 
   return (
@@ -90,18 +99,57 @@ const HomeScreen = () => {
         <SummaryVivianBox onCTAClick={() => goToSettingScreen()} />
         <div className="">
           <FormInput
-            label="Next week's Donation"
-            prompt="50%+ last week"
+            id="weeklyDonationNowInput"
+            label="This week's Donation"
+            prompt="Complete this week's donation"
             prefix="DAI"
-            placeholder="$7.5"
-            defaultValue="$7.5"
+            placeholder="123"
+            inputType="number"
+            defaultValue={0}
             type="choose-next-donation"
+            inputStyleType="fullWidth"
+            onSubmit={async (_amount: number) => {
+              const amount = web3.utils.toWei(_amount.toString(), "ether");
+              const result = await donate(myInfo.walletAddress, amount);
+              console.log(result);
+            }}
           >
-            <div className="radio-button-group">
-              <RadioButton label="$5" />
-              <RadioButton label="$7.5" />
-              <RadioButton label="$10" />
-              <RadioButton label="$20" />
+            <div className="radio-button-group center">
+              {Array(4)
+                .fill(0)
+                .map((_: any, index: number) => {
+                  const initAmount = myInfo?.weeklyDonationAmount ?? 0;
+                  const newAmount = initAmount + 2.5 * index * index;
+                  const percentageIncreaseFromLastAmount = ((newAmount - initAmount) * 100) / initAmount;
+                  return (
+                    <RadioButton
+                      onClick={() => {
+                        // UNSAFE OPERATION (Calling getElementById is not a good practice)
+                        const weeklyDonationAmountInput: any = document.getElementById("weeklyDonationNowInput");
+                        if (!weeklyDonationAmountInput) return;
+                        weeklyDonationAmountInput.value = newAmount;
+
+                        // if ("createEvent" in document) {
+                        //   var evt = document.createEvent("HTMLEvents");
+                        //   evt.initEvent("change", false, true);
+                        //   weeklyDonationAmountInput.dispatchEvent(evt);
+                        // } else {
+                        //   weeklyDonationAmountInput.fireEvent("onchange");
+                        // }
+
+                        var nativeInputValueSetter = Object.getOwnPropertyDescriptor(
+                          window.HTMLInputElement.prototype,
+                          "value",
+                        )?.set;
+                        nativeInputValueSetter?.call(weeklyDonationAmountInput, newAmount);
+                        var ev2 = new Event("input", { bubbles: true });
+                        weeklyDonationAmountInput.dispatchEvent(ev2);
+                      }}
+                      key={index}
+                      label={`${newAmount}`}
+                    />
+                  );
+                })}
             </div>
           </FormInput>
 
