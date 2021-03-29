03e63e4020f4737ff9e4612fe615b8b2af275d47
Parth Raghav <iniento12@gmail.com>
2021-03-29 12:45:38 +0530

    added getMyInfo method and updateMyName method

---
+++ client/src/core/user.ts
@@ -0,0 +1,23 @@
+import { FirebaseApp } from "../firebase";
+
+export const getMyInfo = async () => {
+  const uid = FirebaseApp.auth.currentUser?.uid;
+  if (!uid) return;
+  const userRef = FirebaseApp.db.collection("users").doc(uid);
+  const userDoc = await userRef.get();
+  if (userDoc.exists) {
+    const data = userDoc.data();
+    return data;
+  } else {
+    return;
+  }
+};
+
+export const updateMyName = async (newName: string) => {
+  const uid = FirebaseApp.auth.currentUser?.uid;
+  if (!uid) return;
+  const userRef = FirebaseApp.db.collection("users").doc(uid);
+  await userRef.update({
+    name: newName,
+  });
+};
