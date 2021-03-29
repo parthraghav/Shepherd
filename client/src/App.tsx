6df9c65fc464e25f50fc92b1a0b674744a590eb3
Parth Raghav <iniento12@gmail.com>
2021-03-29 09:56:24 +0530

    added loader state + fixed routing bugs

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,34 +1,72 @@
-import React, { Component } from "react";
-import { BrowserRouter as Router, Switch } from "react-router-dom";
-import { PrivateRoute, ProxyRoute, PublicRoute } from "./routes";
+import React, { useState } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { PrivateRoute, ProxyRoute, PublicOnlyRoute } from "./routes";
 import { HomeScreen, SettingScreen, WelcomeScreen } from "./screens";
+import Screen from "./screens/screen";
 import { Transition } from "react-transition-group";
 import "./theme/styles.css";
 import "./theme/shared.css";
+import { FirebaseApp } from "./firebase";
+import { Loader } from "@components";
+import { LoaderState } from "@components/loader";
+import { logout } from "@core/auth";
 
 const routes = [
-  { path: "/welcome", component: WelcomeScreen, routeType: PublicRoute },
+  { path: "/welcome", component: WelcomeScreen, routeType: PublicOnlyRoute },
   { path: "/home", component: HomeScreen, routeType: PrivateRoute },
   { path: "/settings", component: SettingScreen, routeType: PrivateRoute },
 ];
 
+enum UserStatus {
+  Unknown,
+  LoggedIn,
+  LoggedOut,
+}
+
 function App() {
+  const [user, setUser] = useState(FirebaseApp.auth.currentUser);
+  const [userStatus, setUserStatus] = useState(UserStatus.Unknown);
+  FirebaseApp.auth.onAuthStateChanged(function (user) {
+    setUser(user);
+    if (user) {
+      setUserStatus(UserStatus.LoggedIn);
+    } else {
+      setUserStatus(UserStatus.LoggedOut);
+    }
+  });
+
   return (
     <div className="app">
-      <Router>
-        <Switch>
-          {routes.map(({ path, component: Component, routeType: Route }) => (
-            <Route key={path} exact path={path}>
-              {({ match }: any) => (
-                <Transition in={match !== null} timeout={300} unmountOnExit>
-                  <Component shouldRender={match !== null} />
-                </Transition>
-              )}
-            </Route>
-          ))}
-          <ProxyRoute path="/" to="/welcome" />
-        </Switch>
-      </Router>
+      {userStatus == UserStatus.Unknown ? (
+        <Screen className={"welcome-screen progress"}>
+          <div className="welcome-box">
+            <div></div>
+            <div className="welcome-loader-container">
+              <Loader state={LoaderState.Progress} />
+            </div>
+            <div></div>
+          </div>
+        </Screen>
+      ) : (
+        <Router>
+          <Switch>
+            {routes.map(({ path, component: Component, routeType: CustomRoute }) => (
+              <CustomRoute
+                key={path}
+                exact
+                path={path}
+                user={user}
+                component={({ match }: any) => (
+                  <Transition in={match !== null} timeout={300} unmountOnExit>
+                    <Component />
+                  </Transition>
+                )}
+              ></CustomRoute>
+            ))}
+            <ProxyRoute path="/" to="/welcome" />
+          </Switch>
+        </Router>
+      )}
     </div>
   );
 }
