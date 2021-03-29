7d4081ee9dd9d0a89807c01e180c8b9dbee6ce0f
Parth Raghav <iniento12@gmail.com>
2021-03-29 09:55:42 +0530

    exposed auth triggerr

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,9 +1,12 @@
 import * as CloudFunctions from "firebase-functions";
 import * as admin from "firebase-admin";
 import * as Mirror from "./Mirror";
+import * as Triggers from "./Triggers";
 const functions = CloudFunctions.region("us-central1");
-
 admin.initializeApp();
+
+////////////////////////////////// TRIGGERS //////////////////////////////////
+export const authOnCreate = functions.auth.user().onCreate(Triggers.Auth.onCreate);
 
 ////////////////////////////////// REQUESTS //////////////////////////////////
 export const didContractEmitTransaction = functions.https.onRequest(Mirror.didContractEmitTransaction);
