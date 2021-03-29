7b55946d68a3aea7d3de703d1f1e8ecbe9d44c0e
Parth Raghav <iniento12@gmail.com>
2021-03-29 09:55:25 +0530

    added auth trigger

---
+++ server/src/triggers/auth.ts
@@ -0,0 +1,31 @@
+import * as admin from "firebase-admin";
+import * as functions from "firebase-functions";
+
+export const onCreate = async (user: admin.auth.UserRecord) => {
+  try {
+    // create firebase user reference
+    const userRef = admin.firestore().collection("users").doc(user.uid);
+    const timestamp = admin.firestore.Timestamp.now().toMillis();
+    await userRef.set({
+      id: user.uid,
+      uid: user.uid,
+      walletAddress: "0x0",
+      isWalletAddressProvided: false,
+      createdAt: timestamp,
+      updatedAt: timestamp,
+      name: user.displayName,
+      email: user.email,
+      photoUrl: user.photoURL,
+      isDisabled: user.disabled,
+      phoneNumber: user.phoneNumber,
+      didSetupPaymentMethod: false,
+      demonstratedNeedAmount: 0,
+      demonstratedNeedAmountUpdatedAt: timestamp,
+      weeklyDonationAmount: 5,
+    });
+    return true;
+  } catch (e) {
+    functions.logger.error(e);
+    return false;
+  }
+};
