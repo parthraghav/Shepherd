c9c379eb734397a866563cc83fc84d1c6197f7d3
Parth Raghav <iniento12@gmail.com>
2021-03-30 13:08:08 +0530

    made name and donation input functional

--- client/src/screens/setting_screen.tsx
+++ client/src/screens/setting_screen.tsx
@@ -7,17 +7,23 @@ import { PaymentIcon } from "@components/payment_icon";
 import { useHistory, useLocation } from "react-router-dom";
 import { TimelineMax, TweenMax } from "gsap";
 import { logout } from "@core/auth";
+import { getMyInfo, updateMyName, updateMyWeeklyDonationAmount } from "@core/user";
 
-const WelcomeVivianBox = ({ onCTAClick, ...rest }: any) => (
-  <VivianBox {...rest} icon={faGlobeAmericas} onCTAClick={onCTAClick} primaryColor="2">
-    <span className="text">Welcome back!</span>
-    <div className="heading welcome-heading">
-      <span>Parth</span>
-    </div>
-  </VivianBox>
-);
+const WelcomeVivianBox = ({ onCTAClick, userInfo, ...rest }: any) => {
+  const fullName = userInfo?.name ?? "";
+  const firstName = fullName.split(" ")[0];
+  return (
+    <VivianBox {...rest} icon={faGlobeAmericas} onCTAClick={onCTAClick} primaryColor="2">
+      <span className="text">Welcome back!</span>
+      <div className="heading welcome-heading">
+        <span>{firstName}</span>
+      </div>
+    </VivianBox>
+  );
+};
 
 const SettingScreen = () => {
+  const [myInfo, setMyInfo] = useState<any>();
   const history = useHistory();
   const location: any = useLocation();
   const settingScreenRef: any = useRef<HTMLDivElement>();
@@ -53,13 +59,21 @@ const SettingScreen = () => {
     }
   }, [TweenMax, location]);
 
+  useEffect(() => {
+    (async function () {
+      const fetchedUserInfo = await getMyInfo();
+      setMyInfo(fetchedUserInfo);
+      console.log(fetchedUserInfo);
+    })();
+  }, []);
+
   return (
     <Screen>
       <div ref={settingScreenRef} className="setting-screen" style={{ display: "none" }}>
         <div className="app-header">
           <Logo />
         </div>
-        <WelcomeVivianBox onCTAClick={() => goToHomeScreen()} />
+        <WelcomeVivianBox userInfo={myInfo} onCTAClick={() => goToHomeScreen()} />
         <div className="form-group">
           <FormButton label="Payment Method" prompt="Ending in 3982" hint={<PaymentIcon type="mastercard" />} />
           <FormInput
@@ -67,6 +81,7 @@ const SettingScreen = () => {
             prompt="What’s your need?"
             prefix="USD"
             placeholder="$56"
+            inputStyleType="fullWidth"
             defaultValue="$56"
           />
           <div className="form-paragraph">
@@ -79,8 +94,15 @@ const SettingScreen = () => {
             label="Your Donation"
             prompt="Choose a weekly donation"
             prefix="USD"
-            placeholder="$5"
-            defaultValue="$5"
+            placeholder="123"
+            inputStyleType="fullWidth"
+            inputType="number"
+            defaultValue={myInfo?.weeklyDonationAmount}
+            onSubmit={async (newAmount: string) => {
+              const amount = parseFloat(newAmount);
+              await updateMyWeeklyDonationAmount(amount);
+              window.location.reload();
+            }}
           />
           <div className="radio-button-group">
             <RadioButton label="$5 (same as last week)" />
@@ -89,7 +111,17 @@ const SettingScreen = () => {
             <RadioButton label="$20 (300% more)" />
           </div>
 
-          <FormInput label="Your Name" prompt="What's your name?" placeholder="Parth" defaultValue="Parth" />
+          <FormInput
+            label="Your Name"
+            prompt="What's your name?"
+            placeholder="Your name"
+            defaultValue={myInfo?.name}
+            inputStyleType="fullWidth"
+            onSubmit={async (newName: string) => {
+              await updateMyName(newName);
+              window.location.reload();
+            }}
+          />
         </div>
         <div className="setting-button-group">
           <Button label="Need help?" />
