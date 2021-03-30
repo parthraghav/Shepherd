7bd577538522073cb83d90ed0c78fd223ac16498
Parth Raghav <iniento12@gmail.com>
2021-03-30 13:07:08 +0530

    added functionality to input box

--- client/src/components/input_box.tsx
+++ client/src/components/input_box.tsx
@@ -1,25 +1,25 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCheck } from "@fortawesome/free-solid-svg-icons";
 import React, { useState } from "react";
 import "./styles.css";
 
-export const InputBox = ({
-  prefix,
-  placeholder,
-  defaultValue,
-  ...rest
-}: any) => {
-  const [size, setSize] = useState(placeholder.length);
+export const InputBox = ({ prefix, placeholder, defaultValue, onSubmit, type, ...rest }: any) => {
+  let _defaultValue = defaultValue ?? "";
+  const [value, setValue] = useState(_defaultValue);
+  const [size, setSize] = useState(_defaultValue.length);
   const handleChange = (evt: any) => {
-    setSize(evt.target.value.length || placeholder.length);
+    setSize(evt.target.value.length || _defaultValue.length);
+    setValue(evt.target.value);
   };
   return (
     <div {...rest} className="input-box">
       {prefix != undefined && <span>{prefix}</span>}
-      <input
-        placeholder={placeholder}
-        value={defaultValue}
-        size={size}
-        onChange={handleChange}
-      />
+      <input type={type ?? "text"} placeholder={_defaultValue} value={value} size={size} onChange={handleChange} />
+      {value != _defaultValue && value != "" && (
+        <div className="input-save-button" onClick={() => onSubmit(value)}>
+          <FontAwesomeIcon icon={faCheck} />
+        </div>
+      )}
     </div>
   );
 };
