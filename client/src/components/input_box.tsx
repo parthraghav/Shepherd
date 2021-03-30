fbbfd8fbe31768c9b89031bf6bd527c13eb0c891
Parth Raghav <iniento12@gmail.com>
2021-03-30 15:57:37 +0530

    added select input

--- client/src/components/input_box.tsx
+++ client/src/components/input_box.tsx
@@ -3,7 +3,17 @@ import { faCheck } from "@fortawesome/free-solid-svg-icons";
 import React, { useState } from "react";
 import "./styles.css";
 
-export const InputBox = ({ prefix, placeholder, defaultValue, onSubmit, type, ...rest }: any) => {
+export const InputBox = ({
+  id,
+  prefix,
+  placeholder,
+  defaultValue,
+  valueList,
+  onSubmit,
+  onChange,
+  type,
+  ...rest
+}: any) => {
   let _defaultValue = defaultValue ?? "";
   const [value, setValue] = useState(_defaultValue);
   const [size, setSize] = useState(_defaultValue.length);
@@ -13,12 +23,44 @@ export const InputBox = ({ prefix, placeholder, defaultValue, onSubmit, type, ..
   };
   return (
     <div {...rest} className="input-box">
-      {prefix != undefined && <span>{prefix}</span>}
-      <input type={type ?? "text"} placeholder={_defaultValue} value={value} size={size} onChange={handleChange} />
-      {value != _defaultValue && value != "" && (
-        <div className="input-save-button" onClick={() => onSubmit(value)}>
-          <FontAwesomeIcon icon={faCheck} />
-        </div>
+      {type == "select" ? (
+        <>
+          <select
+            name="select"
+            id="select"
+            className="input-select"
+            value={defaultValue}
+            defaultValue={defaultValue}
+            onChange={evt => {
+              onChange(evt.target.value);
+              handleChange(evt);
+            }}
+          >
+            {valueList &&
+              valueList.map((value: string) => (
+                <option value={value} key={value}>
+                  {value}
+                </option>
+              ))}
+          </select>
+        </>
+      ) : (
+        <>
+          {prefix != undefined && <span>{prefix}</span>}
+          <input
+            type={type ?? "text"}
+            placeholder={_defaultValue}
+            value={value}
+            size={size}
+            onChange={handleChange}
+            id={id}
+          />
+          {value != _defaultValue && value != "" && (
+            <div className="input-save-button" onClick={() => onSubmit(value)}>
+              <FontAwesomeIcon icon={faCheck} />
+            </div>
+          )}
+        </>
       )}
     </div>
   );
