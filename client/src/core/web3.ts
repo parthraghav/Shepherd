a7374156624a9cbebb2df3ff0bbb40d152361e0f
Parth Raghav <iniento12@gmail.com>
2021-03-31 10:39:13 +0530

    added infura endpoint

--- client/src/core/web3.ts
+++ client/src/core/web3.ts
@@ -1,3 +1,6 @@
 import Web3 from "web3";
 
-export const web3 = new Web3(Web3.givenProvider || "http://localhost:3000");
+const LOCAL_PROVIDER = "http://localhost:3000";
+const REMOTE_PROVIDER = "https://kovan.infura.io/v3/79da42feadf04d3a96267c3724f5840d";
+
+export const web3 = new Web3(Web3.givenProvider || REMOTE_PROVIDER);
