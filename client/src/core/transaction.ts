14a8a6c9adb7c6f6c87860dae6a9b23a5083626e
Parth Raghav <iniento12@gmail.com>
2021-03-31 12:35:55 +0530

    added getMyTransactions method

---
+++ client/src/core/transaction.ts
@@ -0,0 +1,20 @@
+import Transaction from "@models/transaction";
+import app from "firebase/app";
+import { FirebaseApp } from "../firebase";
+
+export const getMyTransactions = async () => {
+  const uid = FirebaseApp.auth.currentUser?.uid;
+  if (!uid) return;
+  const transactionsRef = FirebaseApp.db.collection("transactions").where("uid", "in", [uid]);
+  const transactionsSnapshot = await transactionsRef.get();
+  let fetchedTransactions: any = [];
+
+  if (!transactionsSnapshot.empty) {
+    transactionsSnapshot.forEach(doc => {
+      const transaction = doc.data();
+      fetchedTransactions.push(transaction);
+    });
+  }
+
+  return fetchedTransactions;
+};
