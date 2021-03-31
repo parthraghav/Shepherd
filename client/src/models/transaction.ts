727dc6adb90ecb8c5fb25d757a5a277ba5481306
Parth Raghav <iniento12@gmail.com>
2021-03-31 12:36:17 +0530

    added the new interface

--- client/src/models/transaction.ts
+++ client/src/models/transaction.ts
@@ -1,28 +1,56 @@
-type TransactionType = "Donated" | "Received";
-
-type SupportedCurrencyType = "USD" | "EUR" | "GBP" | "INR" | "DAI";
-
-interface ReceivedTransactionType {
-  readonly type: TransactionType;
-  readonly isFirstOfKind?: boolean;
-  readonly donorCount?: number;
-  readonly didMeetDemonstratedNeed?: boolean;
+export enum TransactionType {
+  Donation,
+  Redistribution,
 }
-
-interface SentTransactionType {
-  readonly type: TransactionType;
-  readonly isFirstOfKind?: boolean;
-  readonly redistributionCount?: number;
-  readonly donationMatchCount?: number;
-}
-
-type TransactionSummaryType = ReceivedTransactionType | SentTransactionType;
 
 export default interface Transaction {
-  readonly type: TransactionType;
-  readonly timestamp: number;
-  readonly uuid: string;
-  readonly amount: number;
-  readonly currency: SupportedCurrencyType;
-  readonly summary: TransactionSummaryType;
+  id: string;
+  tid: string;
+  uid: undefined | string;
+  isRegistered: boolean;
+  type: TransactionType;
+  beneficiary: string;
+  remitter: string;
+  emittedAt: number;
+  recordedAt: number;
+  amount: number;
+  currency: string;
+  analytics: Analytics;
 }
+
+interface Analytics {
+  isFirstOfType: boolean;
+  beneficiaryCount: number;
+  remitterCount: number;
+  didMeetDemonstratedNeed: undefined | boolean;
+  percentageIncreaseFromLastTransaction: number;
+}
+
+// type TransactionType = "Donated" | "Received";
+
+// type SupportedCurrencyType = "USD" | "EUR" | "GBP" | "INR" | "DAI";
+
+// interface ReceivedTransactionType {
+//   readonly type: TransactionType;
+//   readonly isFirstOfKind?: boolean;
+//   readonly donorCount?: number;
+//   readonly didMeetDemonstratedNeed?: boolean;
+// }
+
+// interface SentTransactionType {
+//   readonly type: TransactionType;
+//   readonly isFirstOfKind?: boolean;
+//   readonly redistributionCount?: number;
+//   readonly donationMatchCount?: number;
+// }
+
+// type TransactionSummaryType = ReceivedTransactionType | SentTransactionType;
+
+// export default interface Transaction {
+//   readonly type: TransactionType;
+//   readonly timestamp: number;
+//   readonly uuid: string;
+//   readonly amount: number;
+//   readonly currency: SupportedCurrencyType;
+//   readonly summary: TransactionSummaryType;
+// }
